d2ded2db876ea27740899639740599cddb6b82ac
Volodymyr  Krokhmaliuk <volodymyr_krokhmaliuk@Volodymyrs-MacBook-Pro.local>
2022-01-26 19:40:52 +0200

    initial commit

---
+++ src/main/java/lviv/cursor/demo/MvcWithThymeleafeApplication.java
@@ -0,0 +1,13 @@
+package lviv.cursor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcWithThymeleafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcWithThymeleafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lviv/cursor/demo/MvcWithThymeleafeApplicationTests.java
@@ -0,0 +1,13 @@
+package lviv.cursor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcWithThymeleafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
